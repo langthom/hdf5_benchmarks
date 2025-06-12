277e0c1f8a9f538976fa366bf2b81c1ee969b25e
Thomas Lang <langthom@forwiss.uni-passau.de>
2025-06-12 22:33:19 +0200

    remove C++ sources, switching to Python testing instead

--- C_multiple_datasets.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "configuration.h"
-
-void C_multiple_datasets_write(Configuration const& config, double* measurementSecs);
-void C_multiple_datasets_read(Configuration const& config, double* measurementsSecs);
--- C_single_dataset.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "configuration.h"
-
-void C_single_dataset_write(Configuration const& config, double* measurementSecs);
-void C_single_dataset_read(Configuration const& config, double* measurementsSecs);
